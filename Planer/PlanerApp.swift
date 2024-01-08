709b98821b9b05e5c18ed78731333bfbba960161
Jan Phillip Schwietzer <schwietzer.jan@gmail.com>
2024-01-08 18:53:09 +0100

    Initial Commit

---
+++ Planer/PlanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanerApp.swift
+//  Planer
+//
+//  Created by Jan Schwietzer on 08.01.24.
+//
+
+import SwiftUI
+
+@main
+struct PlanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
